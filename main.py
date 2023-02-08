55c8f72525099494db4487e66326da514324843f
Mike Fisher <mikefisher@Mikes-MacBook-Air.local>
2023-02-08 11:54:32 -0500

    add css and html files

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import os
 import openai
 
 from gpt_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
-from flask import Flask, request
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 os.environ.get('OPENAI_API_KEY')
 
 @app.route("/")
 def index():
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
     return '''
         <form action="/answer" method="post">
             What do you want to ask Fish? <input type="text" name="question">
@@ -22,7 +24,8 @@ def answer():
     index = GPTSimpleVectorIndex.load_from_disk('data/index.json')
     question = request.form["question"]
     response = index.query(question, response_mode="compact")
-    return f"Fish Bot says: <b>{response.response}</b>"
+    # return f"Fish Bot says: <b>{response.response}</b>"
+    return redirect(url_for("index", result=response.response))
 
 
 if __name__ == "__main__":
